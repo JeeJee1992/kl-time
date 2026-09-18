f85fcea98f0e8ad797da2274ac4bfa90781c3bed
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-18 16:58:15 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// วาง URL Web app ของ Apps Script ที่ลงท้ายด้วย /exec ระหว่างเครื่องหมายคำพูด
-// ตัวอย่าง: window.KL_BACKEND_URL = 'https://script.google.com/macros/s/ABC123/exec';
 window.KL_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbyhe_z3zv5SSGQadK9YK7U-27xJRzvnwugrHzOAj7zeFpwbybo7AzSzF6ZR7OWBIo-qNQ/exec';
