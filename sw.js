699f1a07d1503751e766e123f4aa666793896d59
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-18 23:54:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kl-time-kiosk-v7-face-offline';
+const CACHE='kl-time-kiosk-v8-offline-auth-fix';
 const FACE_CACHE='kl-time-face-assets-v1';
 const ASSETS=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
