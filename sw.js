16b8db828f5f16e33f69ed7eae5d334db4ef1a80
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-19 13:53:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='kl-time-kiosk-v8-4-fast-queue-gps-warm';
+const CACHE='kl-time-fast-kiosk-pilot-v1-1-pin-fallback';
 const FACE_CACHE='kl-time-face-assets-v1';
-const ASSETS=['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const ASSETS=['./','./index.html','./fast.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k!==FACE_CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
