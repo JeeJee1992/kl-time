df06c1253b5040087971afe47e11c35abe5ba766
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-19 15:47:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kl-time-hr-full-v2-2-timesheet-payroll-sync';
+const CACHE='kl-time-hr-full-v2-3-simple-attendance-rule';
 const FACE_CACHE='kl-time-face-assets-v1';
 const ASSETS=['./','./index.html','./fast.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
