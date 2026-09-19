22f2e319a77d2b63062a423128f383e90abeedbb
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-19 14:52:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kl-time-fast-kiosk-pilot-v1-1-pin-fallback';
+const CACHE='kl-time-hr-full-v2';
 const FACE_CACHE='kl-time-face-assets-v1';
 const ASSETS=['./','./index.html','./fast.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
