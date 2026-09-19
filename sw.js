7281598ba5673e910c8cade5234413b51328ed2e
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-19 16:23:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kl-time-hr-full-v2-5-personal-grid';
+const CACHE='kl-time-hr-full-v2-6-personal-timecard';
 const FACE_CACHE='kl-time-face-assets-v1';
 const ASSETS=['./','./index.html','./fast.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
