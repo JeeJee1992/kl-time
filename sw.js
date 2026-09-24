4c239ef6ae55d7ac1cc87da6a5e1275efe4e0b23
JeeJee1992 <lukchanlynamtan5@gmail.com>
2026-09-24 16:30:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kl-time-v4-2-auto-sync-admin-live';
+const CACHE='kl-time-v4-3-status-reconcile';
 const FACE_CACHE='kl-time-face-assets-v1';
 const ASSETS=['./','./index.html','./fast.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
